11df6a20cb42895c63e6af867ceabb0af3f5168d
masud-k <contact.masud99@gmail.com>
2024-05-03 09:55:51 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <h2 className='text-3xl text-center font-semibold'>Price Options</h2>
+        
+      </div>
+
+    </>
+  )
+}
+
+export default App
